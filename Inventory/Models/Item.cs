4abe7888b32fea1cadf5b2adca8eb5211b5921b0
Pugsly99 <rylandadams@yahoo.com>
2020-12-22 15:51:25 -0800

    saves to database
    Co-authored-by: Max Brockbank <maxbrockbank1999@gmail.com>
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

--- Inventory/Models/Item.cs
+++ Inventory/Models/Item.cs
@@ -1,34 +1,8 @@
-using System.Collections.Generic;
-
 namespace Inventory.Models
 {
   public class Item
   {
+    public int ItemId { get; set; }
     public string Description { get; set; }
-    public int ItemId { get; }
-    private static List<Item> _instances = new List<Item> { };
-
-    public Item(string description)
-    {
-      Description = description;
-      _instances.Add(this);
-      ItemId = _instances.Count;
-    }
-
-    public static List<Item> GetAll()
-    {
-      return _instances;
-    }
-
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
-
-    public static Item Find(int searchId)
-    {
-      return _instances[searchId - 1];
-    }
-
   }
 }
